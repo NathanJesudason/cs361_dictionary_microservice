5aeda7dbd4225c5fee53f01ad7046b4c510af227
Nathan Jesudason <nathan.jesudason@gmail.com>
2021-08-15 08:44:00 -0700

    changed request to axios

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var request = require('request');
+const axios = require('axios');
 let path = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 var port = process.env.PORT || 3000;
 
 app.get('/:query', function(req, res, next){
    var search = req.params.query;
 
-    request((path+search), function(err, response, body){
+/*     request((path+search), function(err, response, body){
         var data = JSON.parse(body);
-        res.status(200).send(data[0].meanings[0].definitions[0].definition);
-    });
+        res.send(data[Object.keys(data)[0]].meanings[0].definitions[0].definition);
+    }); */
+
+
+        axios.get(path + search) 
+        .then(response => {
+        
+            console.log(response.data[Object.keys(response.data)[0]].meanings[0].definitions[0].definition);
+            res.send(JSON.stringify(response.data[Object.keys(response.data)[0]].meanings[0].definitions[0].definition));
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
 
 
 });
